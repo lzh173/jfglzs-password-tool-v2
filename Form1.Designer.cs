2c3ea12c1611b86cf5495d4de52a87f58d8c695c
lzh173 <1294092988@qq.com>
2025-12-14 21:28:27 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace jfglzs_password_tool_v2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            button1 = new Button();
+            radioButton5 = new RadioButton();
+            radioButton4 = new RadioButton();
+            radioButton3 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton1 = new RadioButton();
+            dateTimePicker1 = new DateTimePicker();
+            groupBox2 = new GroupBox();
+            label3 = new Label();
+            button2 = new Button();
+            textBox2 = new TextBox();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(radioButton5);
+            groupBox1.Controls.Add(radioButton4);
+            groupBox1.Controls.Add(radioButton3);
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Controls.Add(dateTimePicker1);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(353, 227);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "临时密码计算";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(149, 80);
+            label2.Name = "label2";
+            label2.Size = new Size(84, 20);
+            label2.TabIndex = 1;
+            label2.Text = "系统特征值";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(239, 77);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(59, 27);
+            textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(137, 165);
+            label1.Name = "label1";
+            label1.Size = new Size(53, 20);
+            label1.TabIndex = 8;
+            label1.Text = "label1";
+            label1.Click += label1_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(173, 110);
+            button1.Name = "button1";
+            button1.Size = new Size(123, 45);
+            button1.TabIndex = 7;
+            button1.Text = "计算临时密码";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // radioButton5
+            // 
+            radioButton5.AutoSize = true;
+            radioButton5.Location = new Point(11, 193);
+            radioButton5.Name = "radioButton5";
+            radioButton5.Size = new Size(67, 24);
+            radioButton5.TabIndex = 5;
+            radioButton5.TabStop = true;
+            radioButton5.Text = "10.1-";
+            radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            radioButton4.AutoSize = true;
+            radioButton4.Location = new Point(11, 161);
+            radioButton4.Name = "radioButton4";
+            radioButton4.Size = new Size(72, 24);
+            radioButton4.TabIndex = 4;
+            radioButton4.TabStop = true;
+            radioButton4.Text = "10.1+";
+            radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(11, 131);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(72, 24);
+            radioButton3.TabIndex = 3;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "11.0+";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(11, 101);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(76, 24);
+            radioButton2.TabIndex = 2;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "11.06-";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(11, 71);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(81, 24);
+            radioButton1.TabIndex = 1;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "11.06+";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(46, 36);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(250, 27);
+            dateTimePicker1.TabIndex = 0;
+            dateTimePicker1.ValueChanged += dateTimePicker1_ValueChanged;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(label3);
+            groupBox2.Controls.Add(button2);
+            groupBox2.Controls.Add(textBox2);
+            groupBox2.Location = new Point(389, 12);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(399, 227);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "密码破解";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(57, 165);
+            label3.Name = "label3";
+            label3.Size = new Size(53, 20);
+            label3.TabIndex = 2;
+            label3.Text = "label3";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(70, 101);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 1;
+            button2.Text = "获取密码并";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(70, 36);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(248, 27);
+            textBox2.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private DateTimePicker dateTimePicker1;
+        private RadioButton radioButton5;
+        private RadioButton radioButton4;
+        private RadioButton radioButton3;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private GroupBox groupBox2;
+        private Label label3;
+        private Button button2;
+        private TextBox textBox2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,267 @@
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace jfglzs_password_tool_v2
+{
+
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Visible = false;
+            string computerName = Environment.MachineName;
+            // 获取最后一个字符的ASCII码
+            byte asciiValue = (byte)computerName[computerName.Length - 1];
+            // 显示ASCII码值
+            textBox1.Text = asciiValue.ToString();
+            radioButton1.Checked = true;
+
+        }
+
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            DateTime currentTime = dateTimePicker1.Value;
+            label1.Visible = true;
+            if (radioButton1.Checked == true)
+            {
+                int temp = currentTime.Month * 159 + currentTime.Day * 357 + int.Parse(textBox1.Text) * 258;
+                label1.Text = DecimalToBase7(temp);
+            }
+            if (radioButton2.Checked == true)
+            {
+                int temp = currentTime.Month * 123 + currentTime.Day * 456 + currentTime.Year * 789 + 111;
+                label1.Text = DecimalToBase8(temp);
+            }
+            if (radioButton3.Checked == true)
+            {
+                int temp = currentTime.Month * 123 + currentTime.Day * 456 + currentTime.Year * 789 + 111;
+                label1.Text = temp.ToString();
+            }
+            if (radioButton4.Checked == true)
+            {
+                int temp = (currentTime.Month * 13 + currentTime.Day * 57 + currentTime.Year * 91) * 16 + 11;
+                string text1 = "8" + temp.ToString();
+                label1.Text = text1;
+            }
+            if (radioButton5.Checked == true)
+            {
+                int temp = (currentTime.Month * 13 + currentTime.Day * 57 + currentTime.Year * 91) * 16;
+                string text1 = "8" + temp.ToString();
+                label1.Text = text1;
+            }
+        }
+        static string DecimalToBase7(int decimalNumber)
+        {
+            if (decimalNumber == 0) return "0";
+
+            bool isNegative = decimalNumber < 0;
+            int number = Math.Abs(decimalNumber);
+            string result = "";
+
+            while (number > 0)
+            {
+                int remainder = number % 7;
+                result = remainder.ToString() + result;
+                number /= 7;
+            }
+
+            return isNegative ? "-" + result : result;
+        }
+        static string DecimalToBase8(int decimalNumber)
+        {
+            if (decimalNumber == 0) return "0";
+
+            bool isNegative = decimalNumber < 0;
+            int number = Math.Abs(decimalNumber);
+            string octalResult = "";
+
+            while (number > 0)
+            {
+                int remainder = number % 8;  // 获取余数
+                octalResult = remainder.ToString() + octalResult;  // 添加到前面
+                number /= 8;  // 除以8继续下一轮
+            }
+
+            return isNegative ? "-" + octalResult : octalResult;
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label3.Text = OptimizedTruncatedForgotIssuer.DecryptTruncatedOptimized(textBox2.Text);
+        }
+    }
+    public class OptimizedTruncatedForgotIssuer
+    {
+        private const string WINDOWS_PATH = "C:\\WINDOWS";
+        private const int TRUNCATE_COUNT = 2;
+
+        // 优化的解密方法：先确定可能的字符范围
+        public static string DecryptTruncatedOptimized(string truncatedEncrypted)
+        {
+            // 由于Base64 + 字符位移的特点，我们可以缩小搜索范围
+            // Base64字符集: A-Z, a-z, 0-9, +, /, =
+            // 经过-10的位移后，可能的字符范围
+
+            // Base64字符的ASCII范围：'A'(65) 到 'z'(122)，加上'+','/','='
+            // 减10后：55-112，还有一些特殊字符
+
+            List<int> likelyFirstChars = new List<int>();
+            List<int> likelyLastChars = new List<int>();
+
+            // 确定可能的字符范围
+            for (int c = 32; c <= 126; c++)
+            {
+                // 反向计算：如果这个字符是加密结果，那么原始Base64字符应该是c+10
+                int originalChar = c + 10;
+
+                // 检查originalChar是否在Base64字符范围内
+                if (IsBase64Char(originalChar))
+                {
+                    likelyFirstChars.Add(c);
+                    likelyLastChars.Add(c);
+                }
+            }
+
+            Console.WriteLine($"优化搜索：从{(126 - 32 + 1) * (126 - 32 + 1)}种组合减少到{likelyFirstChars.Count * likelyLastChars.Count}种");
+
+            // 穷举可能的组合
+            foreach (int firstChar in likelyFirstChars)
+            {
+                foreach (int lastChar in likelyLastChars)
+                {
+                    try
+                    {
+                        string fullEncrypted = $"{(char)firstChar}{truncatedEncrypted}{(char)lastChar}";
+                        string decrypted = DecryptFull(fullEncrypted);
+
+                        if (IsLikelyPlaintext(decrypted))
+                        {
+                            return decrypted;
+                        }
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+                }
+            }
+
+            // 如果优化搜索失败，回退到完整搜索
+            return DecryptTruncatedFallback(truncatedEncrypted);
+        }
+
+        // 回退方法：完整搜索
+        private static string DecryptTruncatedFallback(string truncatedEncrypted)
+        {
+            for (int firstChar = 32; firstChar <= 126; firstChar++)
+            {
+                for (int lastChar = 32; lastChar <= 126; lastChar++)
+                {
+                    try
+                    {
+                        string fullEncrypted = $"{(char)firstChar}{truncatedEncrypted}{(char)lastChar}";
+                        string decrypted = DecryptFull(fullEncrypted);
+                        return decrypted; // 第一个成功的就返回
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+                }
+            }
+
+            throw new ArgumentException("解密失败");
+        }
+
+        // 检查是否为Base64字符
+        private static bool IsBase64Char(int asciiCode)
+        {
+            char c = (char)asciiCode;
+            return (c >= 'A' && c <= 'Z') ||
+                   (c >= 'a' && c <= 'z') ||
+                   (c >= '0' && c <= '9') ||
+                   c == '+' || c == '/' || c == '=';
+        }
+
+        // 检查是否为可能的明文
+        private static bool IsLikelyPlaintext(string text)
+        {
+            if (string.IsNullOrEmpty(text) || text.Length > 1000) // 假设明文不会太长
+                return false;
+
+            // 统计可打印字符比例
+            int printableCount = 0;
+            foreach (char c in text)
+            {
+                if (c >= 32 && c <= 126 || c == '\n' || c == '\r' || c == '\t')
+                    printableCount++;
+            }
+
+            return (double)printableCount / text.Length > 0.95; // 95%以上可打印字符
+        }
+
+        // 完整解密
+        private static string DecryptFull(string encryptedStr)
+        {
+            // 逆向字符位移
+            string base64Str = ReverseRateIssuer(encryptedStr);
+
+            // DES 解密
+            string key = WINDOWS_PATH.Substring(0, 8);
+            string iv = WINDOWS_PATH.Substring(1, 8);
+
+            byte[] encryptedData = Convert.FromBase64String(base64Str);
+            return DESDecrypt(encryptedData, key, iv);
+        }
+
+        private static string ReverseRateIssuer(string input)
+        {
+            char[] chars = input.ToCharArray();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                chars[i] = (char)(chars[i] + 10);
+            }
+            return new string(chars);
+        }
+
+        private static string DESDecrypt(byte[] encryptedData, string key, string iv)
+        {
+            using (DES des = DES.Create())
+            {
+                des.Key = Encoding.UTF8.GetBytes(key);
+                des.IV = Encoding.UTF8.GetBytes(iv);
+                des.Mode = CipherMode.CBC;
+                des.Padding = PaddingMode.PKCS7;
+
+                using (ICryptoTransform decryptor = des.CreateDecryptor())
+                {
+                    byte[] decrypted = decryptor.TransformFinalBlock(encryptedData, 0, encryptedData.Length);
+                    return Encoding.UTF8.GetString(decrypted);
+                }
+            }
+        }
+
+    }
+}
+
